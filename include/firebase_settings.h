4c587f8c34a0ca4a2eaf6102524265f9c53831a8
Mujahid <61884536+engr-mujahidabdullah@users.noreply.github.com>
2023-07-25 16:32:34 +0500

    Firebase write and read functions

---
+++ include/firebase_settings.h
@@ -0,0 +1,11 @@
+#ifndef firebase_settings_H  // Header guard macro
+#define firebase_settings_H  // Define the header guard macro
+
+#include <addons/TokenHelper.h> // Provide the token generation process info.
+#include <addons/RTDBHelper.h>  // Provide the RTDB payload printing info and other helper functions.
+
+#define API_KEY "AIzaSyD9uA1TcJGUoKOb9UR6dUGSLOUBXQb-hHU" // Insert Firebase project API Key
+#define DATABASE_URL "https://esp32-project-644ef-default-rtdb.firebaseio.com/" // Insert RTDB URLefine the RTDB URL */
+
+
+#endif  // End of the header guard macro
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,103 @@
-#include <Arduino.h>
 
-// put function declarations here:
-int cout;
-int count(int delay_);
+//Include libraries here:
+#include <WiFi.h>
+#include <Arduino.h>
+#include <Firebase_ESP_Client.h>
+#include <firebase_settings.h>
+
+//Declare constants here:
+#define WIFI_SSID "EiG-LAB"
+#define WIFI_PASSWORD "12345678"
+
+//Put function declarations here:
+void WiFi_init();
+void firebase_init();
+void firebase_write(String path, String data);
+String firebase_read(String path);
+
+// Intialize Variables Here:
+unsigned long sendDataPrevMillis = 0;
+int count = 0;
+bool signupOK = false;
+
+//Firebase Objects:
+FirebaseData fbda;
+FirebaseAuth auth;
+FirebaseConfig conf;
+
+
 
 void setup() {
-  // put your setup code here, to run once:
+
   Serial.begin(115200);
+  WiFi_init();
+  firebase_init();
 }
 
 void loop() {
-  Serial.print(count(1000));
+
+  firebase_write("test/data", "Apple");
+  vTaskDelay(1000);
+  Serial.print(firebase_read("test/data"));
+
+}
+
+void WiFi_init(){
+  
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+}
+
+void firebase_init(){
+  /* Assign the api key (required) */
+  conf.api_key = API_KEY;
+
+  /* Assign the RTDB URL (required) */
+  conf.database_url = DATABASE_URL;
+
+  /* Sign up */
+  if (Firebase.signUp(&conf, &auth, "", "")){
+    Serial.println("ok");
+    signupOK = true;
+  }
+  else{
+    Serial.printf("%s\n", conf.signer.signupError.message.c_str());
   }
 
-// put function definitions here:
-int count(int delay_){
+  /* Assign the callback function for the long running token generation task */
+  conf.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  
+  Firebase.begin(&conf, &auth);
+  Firebase.reconnectWiFi(true);
+}
 
- cout ++;
- vTaskDelay(delay_);
- return cout;
+void firebase_write(String path, String data){
+  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
+      sendDataPrevMillis = millis();
+      // Write an Int number on the database path test/int
+    if (Firebase.RTDB.setString(&fbda, path, data)){
+      Serial.println("PASSED");
+    }
+    else {
+      Serial.println("FAILED");
+    }
+  }
+}
+
+String firebase_read(String path){
+  if (Firebase.RTDB.getString(&fbda, path)){
+        return fbda.stringData();
+      }
+
+    else {
+        return fbda.errorReason();
+      }
 }
